1fe091e8058573ad161ab96c054843a9a2eb8aa4
ayanski <ayanski@touchlane.com>
2020-03-26 11:19:03 +0300

    Removed unnecessary code

--- BootstrapTests/Empry/TestDoubles/EmptyViewSpy.swift
+++ BootstrapTests/Empry/TestDoubles/EmptyViewSpy.swift
@@ -11,25 +11,13 @@ import UIKit
 @testable import Bootstrap
 
 final class EmptyViewSpy: EmptyView {
-    var invokedDisplayHeaderTitle = false
-    var invokedDisplayHeaderTitleCount = 0
     var invokedDisplayHeaderTitleParameters: (headerTitle: String, Void)?
-    var invokedDisplayHeaderTitleParametersList = [(headerTitle: String, Void)]()
     func display(headerTitle: String) {
-        invokedDisplayHeaderTitle = true
-        invokedDisplayHeaderTitleCount += 1
         invokedDisplayHeaderTitleParameters = (headerTitle, ())
-        invokedDisplayHeaderTitleParametersList.append((headerTitle, ()))
     }
 
-    var invokedDisplayFooterImage = false
-    var invokedDisplayFooterImageCount = 0
     var invokedDisplayFooterImageParameters: (footerImage: UIImage, Void)?
-    var invokedDisplayFooterImageParametersList = [(footerImage: UIImage, Void)]()
     func display(footerImage: UIImage) {
-        invokedDisplayFooterImage = true
-        invokedDisplayFooterImageCount += 1
         invokedDisplayFooterImageParameters = (footerImage, ())
-        invokedDisplayFooterImageParametersList.append((footerImage, ()))
     }
 }
